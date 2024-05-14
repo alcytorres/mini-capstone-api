42eb4f113eb1aa31a58988d4fabd00ec5fda7a13
Alcy Torres <mxalcytorres@gmail.com>
2024-05-14 16:18:19 -0400

    Started jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+  def hello
+    # show data from database to user
+    @product = Product
+    render template: "contacts/show"
+  end
 end
+
+
+# class ContactsController < ApplicationController
+#   def show
+#     # get data from the db
+#     contact = Contact.find_by(id: 3)
+#     render json: {message: contact.first_name}
+#     @contact = Contact.find_by(id: 3)
+#     render template: "contacts/show"
+#   end
+# end
